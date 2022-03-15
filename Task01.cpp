8661d75f07936360f84810c70dde2b13852ce993
Eman Anjum <100797230+EmanAnjum02@users.noreply.github.com>
2022-03-15 22:37:40 +0500

    Add files via upload

--- Task01.cpp
+++ Task01.cpp
@@ -1,11 +1,68 @@
 #include <iostream>
 using namespace std;
+class Heater
+{
+private:
+    int temp;
 
-#define powerOfTwo(n) ((n & (n - 1)) == 0 ? cout<<"YES "<<n<<" is in power of two" : cout<<"NO "<<n<<" is not in power of two")
+public:
+    Heater() : temp(15) {}  //default constructor
+
+    void cooler()
+    {
+        temp = temp + 5;
+    }
+
+    void warmer()
+    {
+        temp = temp - 5;
+    }
+
+    void print()
+    {
+        cout << "Temprature: " << temp << endl;
+    }
+
+    ~Heater()  //destructor
+    {
+        cout << "\nDestructor called!!!" << endl;
+    }
+};
+
 int main()
 {
-    int num;
-    cout << "Please enter a number to check if it is in the power of two:" << endl;
-    cin >> num;
-   powerOfTwo(num);
+    Heater h;
+    bool found = false;
+    int choice;
+    do
+    {
+    cout << "Please enter a choice: ";
+    cin >> choice;
+    if(choice>0&&choice<=4)  //condition to check that the choices must be positive and valid
+    {
+        switch (choice)
+        {
+        case 1:
+            h.print();
+            break;
+
+        case 2:
+            h.cooler();  //call to the cooler function 
+            h.print();
+            break;
+
+        case 3:
+            h.warmer();  //call to the warmer function
+            h.print();
+            break;
+
+        case 4:
+            found = true;
+            cout << "You have sucessfully exited the program " << endl;
+        }
+    }
+    else 
+    cout<<"Invalid choices are not allowed :("<<endl;
+    } while (!found);  //to keep getting a choice from the user until case 4 is choosen
+    return 0;
 }
